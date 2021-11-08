4af59a680cdb7fd78eefacdfe5f788cbbea848bb
Raminta Razauskaite <r.razauskaite@gmail.com>
2021-11-08 19:37:12 +0000

    Updated first commit

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,9 +1,12 @@
 name = "Martha"
 age = 64
+
 daily_budget = 15.84
 day_spending_1 = 2.04
 day_spending_2 = 3.57
 day_spending_3 = 5.62
+
+# Substraction to get leftover
 leftover = daily_budget - day_spending_1 - day_spending_2 - day_spending_3
 
 print(leftover)
