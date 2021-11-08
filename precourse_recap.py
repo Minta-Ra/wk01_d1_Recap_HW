a5d0baa96f1adb54de97846054fc4333dde88a2b
Raminta Razauskaite <r.razauskaite@gmail.com>
2021-11-08 19:25:35 +0000

    First commit

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+name = "Martha"
+age = 64
+daily_budget = 15.84
+day_spending_1 = 2.04
+day_spending_2 = 3.57
+day_spending_3 = 5.62
+leftover = daily_budget - day_spending_1 - day_spending_2 - day_spending_3
+
+print(leftover)
